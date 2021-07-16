18090558106e9461b4e7c7759616aedadf5393c2
yjbae <yjbae@DESKTOP-HGSRDOQ>
2021-07-16 14:15:01 +0900

    inint commit

---
+++ src/main/java/com/gaonpf/GaonpfSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.gaonpf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaonpfSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaonpfSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaonpf/GaonpfSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaonpf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaonpfSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
